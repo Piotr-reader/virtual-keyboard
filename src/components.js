77f4b3c8a8b83718521292376113833d4524bb5a
Piotr-reader <piotr_partsianko@inbox.ru>
2022-05-01 13:31:52 +0300

    feat: implement div wrapper

--- src/components.js
+++ src/components.js
@@ -1,1 +1,7 @@
 
+function createWrapper(element, className) {
+    const newElement = document.createElement(element);
+    newElement.className = className;
+    document.body.prepend(newElement);
+}
+export default { createWrapper }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import components from './components';
+import './styles/style.scss';
 
+const { createWrapper } = components;
+// const wrapperContainer = document.querySelector('.wrapper');
+createWrapper('div', 'wrapper');
